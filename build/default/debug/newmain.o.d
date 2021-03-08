3def140907c435727fc9b8130e3c284f80814ed3
Sudheer Herle <ssherle89@gmail.com>
2021-03-08 13:22:24 +0530

    first commit

---
+++ build/default/debug/newmain.o.d
@@ -0,0 +1,22 @@
+build/default/debug/newmain.o.d build/default/debug/newmain.o: newmain.c \
+ /opt/microchip/xc8/v2.31/avr/avr/include/stdio.h \
+ /opt/microchip/xc8/v2.31/avr/avr/include/inttypes.h \
+ /opt/microchip/xc8/v2.31/avr/lib/gcc/avr/5.4.0/include/stdint.h \
+ /opt/microchip/xc8/v2.31/avr/avr/include/stdint.h \
+ /opt/microchip/xc8/v2.31/avr/lib/gcc/avr/5.4.0/include/stdarg.h \
+ /opt/microchip/xc8/v2.31/avr/lib/gcc/avr/5.4.0/include/stddef.h \
+ /opt/microchip/xc8/v2.31/avr/avr/include/stdlib.h
+
+/opt/microchip/xc8/v2.31/avr/avr/include/stdio.h:
+
+/opt/microchip/xc8/v2.31/avr/avr/include/inttypes.h:
+
+/opt/microchip/xc8/v2.31/avr/lib/gcc/avr/5.4.0/include/stdint.h:
+
+/opt/microchip/xc8/v2.31/avr/avr/include/stdint.h:
+
+/opt/microchip/xc8/v2.31/avr/lib/gcc/avr/5.4.0/include/stdarg.h:
+
+/opt/microchip/xc8/v2.31/avr/lib/gcc/avr/5.4.0/include/stddef.h:
+
+/opt/microchip/xc8/v2.31/avr/avr/include/stdlib.h:
---
+++ build/default/production/newmain.o.d
@@ -0,0 +1,22 @@
+build/default/production/newmain.o.d build/default/production/newmain.o: \
+ newmain.c /opt/microchip/xc8/v2.31/avr/avr/include/stdio.h \
+ /opt/microchip/xc8/v2.31/avr/avr/include/inttypes.h \
+ /opt/microchip/xc8/v2.31/avr/lib/gcc/avr/5.4.0/include/stdint.h \
+ /opt/microchip/xc8/v2.31/avr/avr/include/stdint.h \
+ /opt/microchip/xc8/v2.31/avr/lib/gcc/avr/5.4.0/include/stdarg.h \
+ /opt/microchip/xc8/v2.31/avr/lib/gcc/avr/5.4.0/include/stddef.h \
+ /opt/microchip/xc8/v2.31/avr/avr/include/stdlib.h
+
+/opt/microchip/xc8/v2.31/avr/avr/include/stdio.h:
+
+/opt/microchip/xc8/v2.31/avr/avr/include/inttypes.h:
+
+/opt/microchip/xc8/v2.31/avr/lib/gcc/avr/5.4.0/include/stdint.h:
+
+/opt/microchip/xc8/v2.31/avr/avr/include/stdint.h:
+
+/opt/microchip/xc8/v2.31/avr/lib/gcc/avr/5.4.0/include/stdarg.h:
+
+/opt/microchip/xc8/v2.31/avr/lib/gcc/avr/5.4.0/include/stddef.h:
+
+/opt/microchip/xc8/v2.31/avr/avr/include/stdlib.h:
